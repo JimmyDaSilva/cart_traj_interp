bf5f621e6f6f207e9b616e2fc9d7a0b1f9a0dca9
JimmyDaSilva <jimmy.dasilva@isir.upmc.fr>
2016-11-09 13:48:46 +0100

    First commit

---
+++ include/cart_traj_interp/cart_traj_interp.hpp
@@ -0,0 +1,61 @@
+#ifndef __CART_TRAJ_INTERP_HPP__
+#define __CART_TRAJ_INTERP_HPP__
+
+#include <rtt_ros_kdl_tools/chain_utils.hpp>
+
+#include <rtt/Component.hpp>
+#include <rtt/TaskContext.hpp>
+#include <rtt/InputPort.hpp>
+#include <rtt/OutputPort.hpp>
+#include <moveit_msgs/MoveGroupActionResult.h>
+#include <kdl/chainfksolvervel_recursive.hpp>
+#include <memory>
+#include <iostream>
+
+class CartTrajInterp : public RTT::TaskContext
+{
+public:
+  CartTrajInterp(const std::string& name);
+  virtual ~CartTrajInterp(){}
+
+  bool configureHook();
+  bool startHook();
+  void updateHook();
+  void stopHook();
+protected:
+  // Input ports
+  RTT::InputPort<moveit_msgs::MoveGroupActionResult> port_traj_in_;
+  RTT::InputPort<Eigen::VectorXd> port_joint_position_in_;
+  RTT::InputPort<Eigen::VectorXd> port_joint_velocity_in_;
+  
+  // Current joint position and velocity
+  Eigen::VectorXd joint_position_in_, joint_velocity_in_;
+  
+  // Output ports
+  RTT::OutputPort<KDL::FrameAcc> port_traj_pt_out_;
+  
+  // Chain utils
+  rtt_ros_kdl_tools::ChainUtils arm_;
+  
+  // Trajectory in
+  moveit_msgs::MoveGroupActionResult traj_in_;
+  KDL::JntArrayAcc pt_in_;
+  
+  // Cartesian trajectory point out
+  KDL::FrameAcc traj_pt_out_;
+  
+  // FK solver
+  boost::scoped_ptr<KDL::ChainFkSolverVel_recursive> fk_solver_vel_;
+  
+  // Jdot solver
+  boost::scoped_ptr<KDL::ChainJntToJacDotSolver> jntToJacDotSolver_;
+  
+  // Number of joints
+  int dof_;
+  
+  // Specify which command point needs to be sent
+  int traj_pt_nb_;
+};
+
+ORO_CREATE_COMPONENT(CartTrajInterp)
+#endif
---
+++ src/cart_traj_interp.cpp
@@ -0,0 +1,108 @@
+#include <cart_traj_interp/cart_traj_interp.hpp>
+
+CartTrajInterp::CartTrajInterp(std::string const& name) : TaskContext(name){
+  this->addPort("TrajectoryIn",this->port_traj_in_);
+  this->addPort("JointPosition",this->port_joint_position_in_);
+  this->addPort("JointVelocity",this->port_joint_velocity_in_);
+  this->addPort("TrajectoryPointOut",this->port_traj_pt_out_);
+}
+
+bool CartTrajInterp::configureHook(){
+  // Initialise the model, the internal solvers etc
+  if( !this->arm_.init() )
+  {
+    RTT::log(RTT::Error) << "Could not init chain utils !" << RTT::endlog();
+    return false;
+  }
+  
+  // The number of joints
+  dof_ = this->arm_.getNrOfJoints();
+  
+  // Resize data
+  pt_in_.q.data.resize(dof_);
+  pt_in_.qdot.data.resize(dof_);
+  pt_in_.qdotdot.data.resize(dof_);
+  
+  // Instantiate solvers
+  this->fk_solver_vel_.reset(new KDL::ChainFkSolverVel_recursive(this->arm_.Chain()));
+  this->jntToJacDotSolver_.reset(new KDL::ChainJntToJacDotSolver(this->arm_.Chain()));
+  
+  this->traj_pt_nb_ = 0;
+  
+  return true;
+}
+
+void CartTrajInterp::updateHook(){
+  
+  // If we get a new trajectory update it and start with point 0
+  if(this->port_traj_in_.read(this->traj_in_) != RTT::NoData)
+  {
+    RTT::log(RTT::Info) << "New trajectory" << RTT::endlog();
+    this->traj_pt_nb_ = 0;
+  }  
+
+  // Read the current state of the robot
+  RTT::FlowStatus fp = this->port_joint_position_in_.read(this->joint_position_in_);
+  RTT::FlowStatus fv = this->port_joint_velocity_in_.read(this->joint_velocity_in_);
+  
+  // Return if not giving anything (might happend during startup)
+  if(fp == RTT::NoData || fv == RTT::NoData)
+    return;
+  
+  // Update the chain model
+  this->arm_.setState(this->joint_position_in_,this->joint_velocity_in_);
+  this->arm_.updateModel();
+  
+  // Check trajectory is not empty
+  if (this->traj_in_.result.planned_trajectory.joint_trajectory.points.size() < 1){
+    RTT::log(RTT::Warning) << "Empty trajectory" << RTT::endlog();
+    return;
+  }
+  
+  // Check trajectory is not finished
+  if (this->traj_pt_nb_ > this->traj_in_.result.planned_trajectory.joint_trajectory.points.size() - 1){
+    RTT::log(RTT::Warning) << "All points in the current trajectory have been sent" << RTT::endlog();
+    return;
+  }
+  
+  // Pick current point in trajectory     
+  for(unsigned int i = 0; i < this->dof_; ++i){
+    this->pt_in_.q(i) = this->traj_in_.result.planned_trajectory.joint_trajectory.points[this->traj_pt_nb_].positions[i];
+    this->pt_in_.qdot(i) = this->traj_in_.result.planned_trajectory.joint_trajectory.points[this->traj_pt_nb_].velocities[i] ;
+    this->pt_in_.qdotdot(i) = this->traj_in_.result.planned_trajectory.joint_trajectory.points[this->traj_pt_nb_].accelerations[i] ;
+  }
+  
+  // Compute FK
+  KDL::FrameVel traj_pt_vel;
+  KDL::JntArrayVel pt_in_vel;
+  pt_in_vel.q = this->pt_in_.q;
+  pt_in_vel.qdot = this->pt_in_.qdot;
+  KDL::Twist JdotQdot;
+  
+  // If FK or JdotQdot solver fails start over
+  if ((this->fk_solver_vel_->JntToCart(pt_in_vel, traj_pt_vel) > 0) &&  (this->jntToJacDotSolver_->JntToJacDot(pt_in_vel, JdotQdot) == 0) ){
+      
+    KDL::Jacobian J = this->arm_.getJacobian();
+    KDL::Twist xdotdot;
+    
+    // Xdd = Jd * qd + J * qdd
+    for(unsigned int i = 0; i < 6; ++i )
+      xdotdot(i) = JdotQdot(i) + J.data(i) * this->pt_in_.qdotdot(i);
+    
+    this->traj_pt_out_ = KDL::FrameAcc(traj_pt_vel.GetFrame(), traj_pt_vel.GetTwist(), xdotdot);
+    
+    // Send cartesian trajectory point to the controller
+    this->port_traj_pt_out_.write(this->traj_pt_out_);
+
+    // Increment counter
+    this->traj_pt_nb_++;
+  }
+  
+}
+
+bool CartTrajInterp::startHook(){
+  return true;
+}
+
+void CartTrajInterp::stopHook() {
+}
