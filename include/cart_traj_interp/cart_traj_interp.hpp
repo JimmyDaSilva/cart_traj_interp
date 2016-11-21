639d9e22164db5cebb3bc470d7b15a3563f98b2a
JimmyDaSilva <jimmy.dasilva@isir.upmc.fr>
2016-11-21 14:38:22 +0100

    Adding the trajectory interpolator inside the component Works !!!

--- include/cart_traj_interp/cart_traj_interp.hpp
+++ include/cart_traj_interp/cart_traj_interp.hpp
@@ -9,6 +9,7 @@
 #include <rtt/OutputPort.hpp>
 #include <moveit_msgs/MoveGroupActionResult.h>
 #include <kdl/chainfksolvervel_recursive.hpp>
+#include <kdl/velocityprofile_spline.hpp>
 #include <memory>
 #include <iostream>
 
@@ -22,6 +23,42 @@ public:
   bool startHook();
   void updateHook();
   void stopHook();
+  
+  // Interpolation came from industrial_trajectory_filters package
+  /*
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2013, Southwest Research Institute
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the Southwest Research Institute, nor the names
+ *  of its contributors may be used to endorse or promote products derived
+ *  from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+  bool interpolate(trajectory_msgs::JointTrajectory& trajectory_in, trajectory_msgs::JointTrajectory& trajectory_out);
+  bool interpolatePt(trajectory_msgs::JointTrajectoryPoint & p1,
+                      trajectory_msgs::JointTrajectoryPoint & p2, double time_from_start,
+                      trajectory_msgs::JointTrajectoryPoint & interp_pt);
 protected:
   // Input ports
   RTT::InputPort<moveit_msgs::MoveGroupActionResult> port_traj_in_;
@@ -40,6 +77,7 @@ protected:
   
   // Trajectory in
   moveit_msgs::MoveGroupActionResult traj_in_;
+  trajectory_msgs::JointTrajectory traj_curr_;
   KDL::JntArrayAcc pt_in_;
   
   // Cartesian trajectory point out
@@ -56,6 +94,10 @@ protected:
   
   // Specify which command point needs to be sent
   int traj_pt_nb_;
+  
+  // The time between each point to send to the controller
+  double sample_duration_;
+  
 };
 
 ORO_CREATE_COMPONENT(CartTrajInterp)
--- src/cart_traj_interp.cpp
+++ src/cart_traj_interp.cpp
@@ -6,6 +6,8 @@ CartTrajInterp::CartTrajInterp(std::string const& name) : TaskContext(name){
   this->addPort("JointVelocity",this->port_joint_velocity_in_);
   this->addPort("TrajectoryPointOut",this->port_traj_pt_out_);
   this->addPort("TrajectoryJointOut",this->port_traj_joint_out_);
+  
+  this->addProperty("SampleDuration",this->sample_duration_).doc("The time between each point to send");
 }
 
 bool CartTrajInterp::configureHook(){
@@ -30,6 +32,15 @@ bool CartTrajInterp::configureHook(){
   
   this->traj_pt_nb_ = 0;
   
+  // Sample duration default value
+  this->sample_duration_ = 0.001;
+  
+  // Match all properties (defined in the constructor) 
+  // with the rosparams in the namespace : 
+  // nameOfThisComponent/nameOftheProperty
+  // Equivalent to ros::param::get("CartOptCtrl/P_gain");
+  rtt_ros_kdl_tools::getAllPropertiesFromROSParam(this);
+  
   return true;
 }
 
@@ -43,7 +54,12 @@ void CartTrajInterp::updateHook(){
   // If we get a new trajectory update it and start with point 0
   if(ftraj == RTT::NewData)
   {
-    RTT::log(RTT::Warning) << "\n\n\n New trajectory with "<<traj_in_.result.planned_trajectory.joint_trajectory.points.size() <<" points \n\n\n" << RTT::endlog();
+    RTT::log(RTT::Info) << "New trajectory with "<<traj_in_.result.planned_trajectory.joint_trajectory.points.size() <<" points" << RTT::endlog();
+    if(!this->interpolate(traj_in_.result.planned_trajectory.joint_trajectory, traj_curr_)){
+      RTT::log(RTT::Error) << "Interpolation of the trajectory failed" << RTT::endlog();
+      return;
+    }
+    RTT::log(RTT::Info) << "Interpolated traj with "<<traj_curr_.points.size() <<" points" << RTT::endlog();
     this->traj_pt_nb_ = 0;
   }
 
@@ -56,22 +72,18 @@ void CartTrajInterp::updateHook(){
   this->arm_.updateModel();
   
   // Check trajectory is not empty
-  if (this->traj_in_.result.planned_trajectory.joint_trajectory.points.size() < 1){
-//     RTT::log(RTT::Warning) << "Empty trajectory" << RTT::endlog();
+  if (this->traj_curr_.points.size() < 1)
     return;
-  }
   
   // Check trajectory is not finished
-  if (this->traj_pt_nb_ > this->traj_in_.result.planned_trajectory.joint_trajectory.points.size() - 1){
-//     RTT::log(RTT::Warning) << "All points in the current trajectory have been sent" << RTT::endlog();
+  if (this->traj_pt_nb_ > this->traj_curr_.points.size() - 1)
     return;
-  }
   
   // Pick current point in trajectory     
   for(unsigned int i = 0; i < this->dof_; ++i){
-    this->pt_in_.q(i) = this->traj_in_.result.planned_trajectory.joint_trajectory.points[this->traj_pt_nb_].positions[i];
-    this->pt_in_.qdot(i) = this->traj_in_.result.planned_trajectory.joint_trajectory.points[this->traj_pt_nb_].velocities[i] ;
-    this->pt_in_.qdotdot(i) = this->traj_in_.result.planned_trajectory.joint_trajectory.points[this->traj_pt_nb_].accelerations[i] ;
+    this->pt_in_.q(i) = this->traj_curr_.points[this->traj_pt_nb_].positions[i];
+    this->pt_in_.qdot(i) = this->traj_curr_.points[this->traj_pt_nb_].velocities[i] ;
+    this->pt_in_.qdotdot(i) = this->traj_curr_.points[this->traj_pt_nb_].accelerations[i] ;
   }
   
   // Compute FK
@@ -99,9 +111,91 @@ void CartTrajInterp::updateHook(){
 
     // Increment counter
     this->traj_pt_nb_++;
-    
   }
-  
+}
+
+bool CartTrajInterp::interpolate(trajectory_msgs::JointTrajectory & trajectory_in, trajectory_msgs::JointTrajectory & trajectory_out)
+{
+  size_t size_in = trajectory_in.points.size();
+  double duration_in = trajectory_in.points.back().time_from_start.toSec();
+  double interpolated_time = 0.0;
+  size_t index_in = 0;
+
+  trajectory_msgs::JointTrajectoryPoint p1, p2, interp_pt;
+
+  trajectory_out = trajectory_in;
+  trajectory_out.points.clear();
+
+  while (interpolated_time < duration_in)
+  {
+    // Increment index until the interpolated time is past the start time.
+    while (interpolated_time > trajectory_in.points[index_in + 1].time_from_start.toSec())
+    {
+      index_in++;
+      if (index_in >= size_in)
+        return false;
+
+    }
+    p1 = trajectory_in.points[index_in];
+    p2 = trajectory_in.points[index_in + 1];
+    if (!interpolatePt(p1, p2, interpolated_time, interp_pt))
+      return false;
+      
+    trajectory_out.points.push_back(interp_pt);
+    interpolated_time += sample_duration_;
+  }
+  p2 = trajectory_in.points.back();
+  p2.time_from_start = ros::Duration(interpolated_time);
+  trajectory_out.points.push_back(p2);
+  return true;
+}
+
+
+bool CartTrajInterp::interpolatePt(trajectory_msgs::JointTrajectoryPoint & p1,
+                                            trajectory_msgs::JointTrajectoryPoint & p2, double time_from_start,
+                                            trajectory_msgs::JointTrajectoryPoint & interp_pt)
+{
+  double p1_time_from_start = p1.time_from_start.toSec();
+  double p2_time_from_start = p2.time_from_start.toSec();
+
+  if (time_from_start >= p1_time_from_start && time_from_start <= p2_time_from_start)
+  {
+    if (p1.positions.size() == p1.velocities.size() && p1.positions.size() == p1.accelerations.size())
+    {
+      if (p1.positions.size() == p2.positions.size() && p1.velocities.size() == p2.velocities.size()
+          && p1.accelerations.size() == p2.accelerations.size())
+      {
+        // Copy p1 to ensure the interp_pt has the correct size vectors
+        interp_pt = p1;
+
+        KDL::VelocityProfile_Spline spline_calc;
+
+        for (size_t i = 0; i < p1.positions.size(); ++i)
+        {
+          // Calculated relative times for spline calculation
+          double time_from_p1 = time_from_start - p1.time_from_start.toSec();
+          double time_from_p1_to_p2 = p2_time_from_start - p1_time_from_start;
+
+          spline_calc.SetProfileDuration(p1.positions[i], p1.velocities[i], p1.accelerations[i], p2.positions[i],
+                                          p2.velocities[i], p2.accelerations[i], time_from_p1_to_p2);
+
+          ros::Duration time_from_start_dur(time_from_start);
+
+          interp_pt.time_from_start = time_from_start_dur;
+          interp_pt.positions[i] = spline_calc.Pos(time_from_p1);
+          interp_pt.velocities[i] = spline_calc.Vel(time_from_p1);
+          interp_pt.accelerations[i] = spline_calc.Acc(time_from_p1);
+        }
+        return true;
+      }
+      else
+        return false;
+    }
+    else
+      return false;
+  }
+  else
+    return false;
 }
 
 bool CartTrajInterp::startHook(){
